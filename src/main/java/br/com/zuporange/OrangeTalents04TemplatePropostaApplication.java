b8b8093a102ff03d361c1e68686ab96b11157a9a
Douglas Rodrigues Fideles <47896736+douglasfideles@users.noreply.github.com>
2021-05-12 18:16:45 -0300

    inicio do projeto

---
+++ src/main/java/br/com/zuporange/OrangeTalents04TemplatePropostaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zuporange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeTalents04TemplatePropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeTalents04TemplatePropostaApplication.class, args);
+	}
+
+}
