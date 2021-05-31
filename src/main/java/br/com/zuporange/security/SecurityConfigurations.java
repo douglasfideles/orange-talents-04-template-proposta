3ecb5ac492ea06e737778efb80f510b8d9f9c497
Douglas Rodrigues Fideles <47896736+douglasfideles@users.noreply.github.com>
2021-05-31 11:17:00 -0300

    050.login_via_senha

--- src/main/java/br/com/zuporange/security/SecurityConfigurations.java
+++ src/main/java/br/com/zuporange/security/SecurityConfigurations.java
@@ -1,31 +1,31 @@
 package br.com.zuporange.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2ClientConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(auth);
-	}
 	
+	
+	//Autorização
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
-	}
-	
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(web);
+		http.authorizeRequests(authorizeRequests ->
+        authorizeRequests
+                
+                .antMatchers(HttpMethod.POST, "/propostas/**").hasAuthority("SCOPE_propostaconnect-scope")
+                .anyRequest().authenticated()
+		)
+		.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 	}
 }
+	
+
